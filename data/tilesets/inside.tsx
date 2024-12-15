fff981cbe75e396bba817d5a9f855ae48cce045f
St2p4n <dracho06@gmail.com>
2024-12-15 23:07:54 +0100

    Initial commit

---
+++ data/tilesets/inside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="inside" tilewidth="64" tileheight="64" tilecount="96" columns="12">
+ <image source="../../png/Tiletst/inside.png" width="768" height="512"/>
+</tileset>
---
+++ data/tilesets/map objects.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="map objects" tilewidth="100" tileheight="128" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../png/Map/objects/palm.png" width="100" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="../../png/Map/objects/grass1.png" width="39" height="24"/>
+ </tile>
+ <tile id="2">
+  <image source="../../png/Map/objects/grass1.png" width="39" height="24"/>
+ </tile>
+ <tile id="3">
+  <image source="../../png/Map/objects/grass3.png" width="28" height="20"/>
+ </tile>
+ <tile id="4">
+  <image source="../../png/Map/objects/grass4.png" width="32" height="20"/>
+ </tile>
+ <tile id="5">
+  <image source="../../png/Map/objects/grass5.png" width="36" height="20"/>
+ </tile>
+ <tile id="6">
+  <image source="../../png/Overworld/objects/stone.png" width="56" height="52"/>
+ </tile>
+</tileset>
