bf1ba7c8dcd22a759e4fe36963bc6e1710c9094a
St2p4n <dracho06@gmail.com>
2025-02-07 16:40:35 +0100

    update

--- Python videogame/level.py
+++ Python videogame/level.py
@@ -5,14 +5,12 @@ class Level:
        self.display_surface = pygame.display.get_surface()
 
     def run(self):
-       #background = pygame.image.load("data/Aseprite/background.png")
-       #self.display_surface.blit(background, (0,0))
+       
+       background = pygame.image.load("data/Levels/background.png")
+       self.display_surface.blit(background, (0,0))
 
-       player = pygame.image.load("data/Aseprite/Pirate.png")
-       self.display_surface.blit(player, (100,100))
-
-      #crab = pygame.image.load("data/Aseprite/Crab.png")
-       #self.display_surface.blit(crab, (200,200))
+       #player = pygame.image.load("data/Aseprite/Pirate.png")
+       #self.display_surface.blit(player, (100,100))
 
 
      
